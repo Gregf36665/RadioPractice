9a3c8630194fb18e2332d5efb57cfaaae5095f21
craigLombardo <clombardo4444@yahoo.com>
2014-05-15 13:49:15 -0400

    Revert a24b6b9..c363e60
    This rolls back to commit a24b6b98e5d28ed5a62013fe0d8b64352b01291c.

--- pcon.js
+++
@@ -1,3 +0,0 @@
-var pcon=function(){
-fast([0,1,2,3,0]);
-}
--- toggle.js
+++ toggle.js
@@ -1,220 +1,34 @@
-var fast=function(mOrder){
-
+var fast=function(){
 var selected=true;
 var choice = document.choice; //simplify the code
 var sign=false;
 var station=false;
 var request=false;
 var rules=false;
-var emergency=false;
-var out=[];
-var din="";
-var order=mOrder;
-
-//order
-/*  0 = callsign
-	1 = station
-	2 = request
-	3 = flight rules
-	4 = nature of emergency
-	5 = Mayday Mayday Mayday
-*/
-	
-
-for(var i=0;i<choice.callsign.length; i++){
-    if(choice.callsign[i].checked){
-		out.push(choice.callsign[i].value);
-		sign=true;
-		}
-	}if(!sign) alert("You need a callsign!");
-
-for(var i=0;i<choice.station.length; i++){
-	if(choice.station[i].checked){
-		out.push(choice.station[i].value);
-		station=true;
-		}
-	}if(!station) out.push("UNICOM");
-
-for(var i=0;i<choice.request.length; i++){
-	if(choice.request[i].checked){
-		out.push(choice.request[i].value);
-		request=true;
-		}
-	}if(!request) out.push("");
-
-
-for(var i=0;i<choice.rules.length; i++){
-	if(choice.rules[i].checked){
-		out.push(choice.rules[i].value);
-		rules=true;
-		}
-	}if(!rules) out.push("");
-
-for(var i=0;i<choice.emergency.length; i++){
-    if(choice.emergency[i].checked){
-		out.push(choice.emergency[i].value);
-		emergency=true;
-		request = true; // emergencies count as requests
-		order = [5,1,0,4];
-		}
-	}if(!emergency) out.push("");
-
-out.push("SOS"); 	
-				
-if(sign && station && request){
-	for(j=0;j<order.length;j++){
-		switch(out[order[j]].toUpperCase()){
-		// emergencies
-			case "SOS":
-				din+="mayday mayday mayday ";
-				break;
-			case "ENGINE":
-				din+="Engine failure commencing forced landing ";
-				break;
-			case "RADIOFAIL":
-				din+="Radio failure returning to November 8 7";
-				break;
-					
-		// callsigns
-			case "N410T":
-				din+="November Four One Zero Tango ";
-				break;
-			case "GBYFA":
-				din+="Golf Bravo Yankee Foxtrot Alpha ";
-				break;
-				
-		// stations
-			case "TOWER":
-				din+="Seattle Tower ";
-				break;
-			case "APPROACH":
-				din+="Seattle Approach ";
-				break;
-			case "UNICOM":
-				din+="Seattle Unicom ";
-				break;
-			case "RADIO":
-				din+="Seattle Radio   ";
-				break;
-			case "FLIGHTWATCH":
-				din+="Seattle Flight Watch   ";
-				break;
-			case "CLEARANCEDELIVERY":
-				din+="Seattle Clearance Delivery   ";
-				break;
-			case "GROUND":
-				din+="Seattle Ground   ";
-				break;
-			case "CENTER":
-				din+="Seattle Center   ";
-				break;
-			case "DEPARTURE":
-				din+="Seattle Departure   ";
-				break;
-				
-		// requests 
-			case "TAXI":
-				din+="Request Taxi   ";
-				break;
-			case "DEPARTURE":
-				din+="Request Departure   ";
-				break;
-			case "FREQUENCYCHANGE":
-				din+="Request Frequency Change   ";
-				break;
-			case "FIRSTCONTACT":
-				din+="";
-				order = []; // this stops anything else from being said
-				break;
-			case "EMERGENCY":
-				din+="mayday mayday mayday";
-				break;
-			case "REJOIN":
-				din+="Request Rejoin   ";
-				break;
-			case "FINALS":
-				din+="Finals for Runway two four left ";
-				break;
-			case "BASE":
-				din+="Base for Runway three four right  ";
-				break;
-			case "DOWNWIND":
-				din+="Down Wind to land runway zero niner ";
-				break;
-			case "VFR":
-				din+="VFR flight to Kilo Sierra Bravo Delta "; // we want to in the end
-				// replace this with a changing name such as another staton
-				break;
-			case "IFR":
-				din+="IFR flight to Kilo Sierra Bravo Delta ";
-				break;
-			case "LOCAL":
-				din+="local flight ";
-				break;
-			}
-		}
-		
-//alert(din); // this is for debugging to see what will get said
-
-var msg = new SpeechSynthesisUtterance();
-var voices = window.speechSynthesis.getVoices();
-msg.voice = voices[0]; // Note: some voices don't support altering params
-msg.voiceURI = 'native';
-msg.volume = 1; // 0 to 1
-msg.rate = 1; // 0.1 to 10
-msg.pitch = 1; //0 to 2
-msg.text = din;
-msg.lang = 'en-US';
-
-speechSynthesis.speak(msg);}
-else {alert("You didn't select them all");}
-};
-
-// What if we want ATCs response?
-
-/*var ATC=function(){
-
-var selected=true;
-var choice = document.choice; //simplify the code
-var sign=false;
-var station=false;
-var request=false;
-var rules=false;
-var emergency=false;
 var out=[];
 var din="";
 var order=[1,0,2,3,0];
 
-//order
-  0 = callsign
-	1 = station
-	2 = request
-	3 = flight rules
-	4 = nature of emergency
-	5 = Mayday Mayday Mayday
-*/
-	
-
 for(var i=0;i<choice.callsign.length; i++){
     if(choice.callsign[i].checked){
 		out.push(choice.callsign[i].value);
 		sign=true;
 		}
-	}if(!sign) alert("You need a callsign!");
+	}
 
 for(var i=0;i<choice.station.length; i++){
 	if(choice.station[i].checked){
 		out.push(choice.station[i].value);
 		station=true;
 		}
-	};
+	}
 
 for(var i=0;i<choice.request.length; i++){
 	if(choice.request[i].checked){
 		out.push(choice.request[i].value);
 		request=true;
 		}
-	}if(!request) out.push("");
+	}
 
 
 for(var i=0;i<choice.rules.length; i++){
@@ -222,113 +36,77 @@ for(var i=0;i<choice.rules.length; i++){
 		out.push(choice.rules[i].value);
 		rules=true;
 		}
-	}if(!rules) out.push("");
-
-for(var i=0;i<choice.emergency.length; i++){
-    if(choice.emergency[i].checked){
-		out.push(choice.emergency[i].value);
-		emergency=true;
-		request = true; // emergencies count as requests
-		order = [5,1,0,4];
-		}
-	}if(!emergency) out.push("");
-
-out.push("SOS"); 	
-				
+	else{out.push("")};
+	}			
 if(sign && station && request){
-	for(j=0;j<order.length;j++){
-		switch(out[order[j]].toUpperCase()){
-		// emergencies
-			case "SOS":
-				din+="mayday mayday mayday ";
-				break;
-			case "ENGINE":
-				din+="Engine failure commencing forced landing ";
-				break;
-			case "RADIOFAIL":
-				din+="Radio failure returning to November 8 7";
-				break;
-					
-		// callsigns
-			case "N410T":
-				din+="November Four One Zero Tango ";
-				break;
-			case "GBYFA":
-				din+="Golf Bravo Yankee Foxtrot Alpha ";
-				break;
-				
-		// stations
-			case "TOWER":
-				din+="Seattle Tower ";
-				break;
-			case "APPROACH":
-				din+="Seattle Approach ";
-				break;
-			case "UNICOM":
-				din+="Seattle Unicom ";
-				break;
-			case "RADIO":
-				din+="Seattle Radio   ";
-				break;
-			case "FLIGHTWATCH":
-				din+="Seattle Flight Watch   ";
-				break;
-			case "CLEARANCEDELIVERY":
-				din+="Seattle Clearance Delivery   ";
-				break;
-			case "GROUND":
-				din+="Seattle Ground   ";
-				break;
-			case "CENTER":
-				din+="Seattle Center   ";
-				break;
-			case "DEPARTURE":
-				din+="Seattle Departure   ";
-				break;
-				
-		// requests 
-			case "TAXI":
-				din+="Request Taxi   ";
-				break;
-			case "DEPARTURE":
-				din+="Request Departure   ";
-				break;
-			case "FREQUENCYCHANGE":
-				din+="Request Frequency Change   ";
-				break;
-			case "FIRSTCONTACT":
-				din+="";
-				order = []; // this stops anything else from being said
-				break;
-			case "EMERGENCY":
-				din+="mayday mayday mayday";
-				break;
-			case "REJOIN":
-				din+="Request Rejoin   ";
-				break;
-			case "FINALS":
-				din+="Finals for Runway two four left ";
-				break;
-			case "BASE":
-				din+="Base for Runway three four right  ";
-				break;
-			case "DOWNWIND":
-				din+="Down Wind to land runway zero niner ";
-				break;
-			case "VFR":
-				din+="VFR flight to Kilo Sierra Bravo Delta "; // we want to in the end
-				// replace this with a changing name such as another staton
-				break;
-			case "IFR":
-				din+="IFR flight to Kilo Sierra Bravo Delta ";
-				break;
-			case "LOCAL":
-				din+="local flight ";
-				break;
-			}
-		}
-		
-//alert(din); // this is for debugging to see what will get said
+for(j=0;j<order.length;j++){
+switch(out[order[j]].toUpperCase()){
+case "N410T":
+	din+="November Four One Zero Tango ";
+	break;
+case "GBYFA":
+	din+="Golf Bravo Yankee Foxtrot Alpha ";
+	break;
+case "TOWER":
+	din+="Seattle Tower ";
+	break;
+case "APPROACH":
+	din+="Seattle Approach ";
+	break;
+case "UNICOM":
+	din+="Seattle Unicom for ";
+	break;
+case "RADIO":
+	din+="Seattle Radio for ";
+	break;
+case "FLIGHTWATCH":
+	din+="Seattle Flight Watch for ";
+	break;
+case "CLEARANCEDELIVERY":
+	din+="Seattle Clearance Delivery for ";
+	break;
+case "GROUND":
+	din+="Seattle Ground for ";
+	break;
+case "CENTER":
+	din+="Seattle Center for ";
+	break;
+case "DEPARTURE":
+	din+="Seattle Departure for ";
+	break;
+case "TAXI":
+	din+="Request Taxi for ";
+	break;
+case "DEPARTURE":
+	din+="Request Departure for ";
+	break;
+case "FREQUENCYCHANGE":
+	din+="Request Frequency Change for ";
+	break;
+case "FIRSTCONTACT":
+	din+="Request First Contact for ";
+	break;
+case "EMERGENCY":
+	din+="Request Emergency for ";
+	break;
+case "REJOIN":
+	din+="Request Rejoin for ";
+	break;
+case "FINALS":
+	din+="Request Finals for ";
+	break;
+case "BASE":
+	din+="Request Base for ";
+	break;
+case "DOWNWIND":
+	din+="Request Down Wind for ";
+	break;
+case "VFR":
+	din+="VFR flight to Kilo Sierra Bravo Delta "; // we want to in the end replace this
+	//with a changing name such as another staton
+	break;
+}}
+alert(din); // this is for debugging to see what will get said
 
 var msg = new SpeechSynthesisUtterance();
 var voices = window.speechSynthesis.getVoices();
